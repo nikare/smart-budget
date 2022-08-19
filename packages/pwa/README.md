5d560859206640b3a5e2ef67d068c35270898145
Nikolay Karetskiy <lynxloner@icloud.com>
2022-08-20 00:33:52 +0200

    Init setup pwa

---
+++ packages/pwa/README.md
@@ -0,0 +1,3 @@
+# Smart Budget
+
+Progressive Web Application
--- packages/pwa/src/index.tsx
+++ packages/pwa/src/index.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { App } from './App';
+
+const Root = (
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+const rootEl = document.getElementById('root');
+if (!rootEl) throw new Error('Root element not found');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(rootEl).render(Root);
+serviceWorkerRegistration.unregister();
