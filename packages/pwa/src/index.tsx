03e86fb734adad808629290289259dc0cd1e622c
Nikolay Karetskiy <lynxloner@icloud.com>
2022-08-20 19:37:13 +0200

    Added gh-pages

--- packages/pwa/src/index.tsx
+++ packages/pwa/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '@smart-budget/vendors';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { App } from './App';
