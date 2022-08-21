bcf0778e4bb4a33ca950dd8710f1b5aa205bee9f
Nikolay Karetskiy <lynxloner@icloud.com>
2022-08-21 11:04:37 +0200

    Added vendor styles

--- packages/pwa/src/index.tsx
+++ packages/pwa/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './vendor';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { App } from './App';
